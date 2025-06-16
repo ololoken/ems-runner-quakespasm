e0211dc41f92a0d6d7c6ad25a142ce95b5f1c9c9
Roman Turchin <roman@turch.in>
2025-06-16 21:28:08 +0200

    final cleanups

--- src/hooks/useYSDK.ts
+++
@@ -1,5 +0,0 @@
-import YaGamesContext from '../contexts/YaGamesContext';
-import { useContext } from 'react';
-import throwExpression from '../common/throwExpression';
-
-export default () => useContext(YaGamesContext) ?? throwExpression(new Error('YaGamesSDK context must be use inside provider'));
